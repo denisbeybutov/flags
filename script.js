02066739f03f98c08eb58fdc4cac3a7ae0b751c9
denisbeybutov <beybutov.denis@mail.ru>
2026-03-11 14:16:40 +0300

    добавил светлую тему и переключение

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.querySelector('.search__button').addEventListener('click', function() {
     this.classList.toggle('active')
 })
+
+document.querySelector('.header__theme-switcher').addEventListener('click', function() {
+    document.querySelector('body').classList.toggle('dark-mode')
+})
